e579886fa35ef3fb3f9d238d91709c903582149d
jsapkota <36951385+jsapkota@users.noreply.github.com>
2018-03-04 11:08:01 +0545

    Create config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', 'localhost6');
+   define('DB_USERNAME', 'mywebappadmin');
+   define('DB_PASSWORD', 'mywebappadmin');
+   define('DB_DATABASE', 'mywebappdb');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
